03d3a1b7cb3f1e1539ff412fd7494743644fdac9
GiselaCS <80045555+GiselaCS@users.noreply.github.com>
2021-03-25 15:17:25 -0500

    Add files via upload

---
+++ BD.sql
@@ -0,0 +1,10 @@
+create database FormularioLanding;
+
+use FormularioLanding;
+
+create table contacto(nombre varchar(50), apellido varchar(50), correo varchar(50), comentario varchar(200));
+
+
+
+select * from contacto;
+
